b40e0cdcadef406dc78473bb276497bbb8841281
dugonzal <ciclocontinuo2@gmail.com>
2022-08-06 10:00:31 +0000

    server client

--- src/client/__init__.py
+++ src/client/__init__.py
@@ -6,20 +6,13 @@
 #    By: vscode <vscode@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/06 09:31:02 by vscode            #+#    #+#              #
-#    Updated: 2022/08/06 09:34:01 by vscode           ###   ########.fr        #
+#    Updated: 2022/08/06 09:59:26 by vscode           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-# Python program to create Blockchain
-# For timestamp
-import datetime
-
-# Calculating the hash
-# in order to add digital
-# fingerprints to the blocks
-import hashlib
-
-# To store data
-# in our blockchain
-
+# Flask is for creating the web
+# app and jsonify is for
+# displaying the blockchain
+from flask import Flask, jsonify
+import jsons as json
 
---
+++ src/server/__init__.py
@@ -0,0 +1,13 @@
+
+# Python program to create Blockchain
+# For timestamp
+import datetime
+
+# Calculating the hash
+# in order to add digital
+# fingerprints to the blocks
+import hashlib
+
+# To store data
+# in our blockchain
+
