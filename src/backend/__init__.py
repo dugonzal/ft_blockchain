0baedf6e67a2ca32c56198747b918f6a2a28091e
dugonzal <ciclocontinuo2@gmail.com>
2022-08-06 09:42:31 +0000

    first commit

---
+++ src/backend/__init__.py
@@ -0,0 +1,25 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    __init__.py                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: vscode <vscode@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/08/06 09:31:02 by vscode            #+#    #+#              #
+#    Updated: 2022/08/06 09:34:01 by vscode           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# Python program to create Blockchain
+# For timestamp
+import datetime
+
+# Calculating the hash
+# in order to add digital
+# fingerprints to the blocks
+import hashlib
+
+# To store data
+# in our blockchain
+
+
---
+++ src/frontend/__init__.py
@@ -0,0 +1,5 @@
+# Flask is for creating the web
+# app and jsonify is for
+# displaying the blockchain
+from flask import Flask, jsonify
+import jsons as json
